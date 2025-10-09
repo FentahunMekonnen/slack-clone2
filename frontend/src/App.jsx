cefc312ba90a9f5379ef4b6f140412babd45d110
Fentahunmekonnen <fentahunmekonnen@gmail.com>
2025-10-09 10:17:27 -0500

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,20 @@ import React from "react";
 
 function App() {
   return (
+<<<<<<< Updated upstream
     <>
       <h1 class="text-3xl font-bold underline">Vite + React</h1>
     </>
+=======
+    <header className="absolute top-0 left-0 w-full p-4 flex justify-end bg-black text-white">
+      <SignedOut>
+        <SignInButton mode="modal" />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </header>
+>>>>>>> Stashed changes
   );
 }
 
